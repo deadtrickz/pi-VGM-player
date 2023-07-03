128640267506e66109e5b70c237f55bf9751203d
deadtrickz <40606419+deadtrickz@users.noreply.github.com>
2023-07-03 00:38:01 -0400

    v1.5.0

--- install_script.sh
+++ install_script.sh
@@ -11,32 +11,34 @@ echo "Updating package lists..."
 sudo apt-get update
 sleep 5
 
-# Install Python3, MOC and Screen
+# Install Python3, MOC and Screen if not already installed
 echo "Installing Python3, MOC, and Screen..."
 sudo apt-get install -y python3 moc screen unzip libao-dev libdbus-1-dev moc-ffmpeg-plugin
 sleep 5
 
-# Create user 'music' with password PVGMP4F and adding it to appropriate groups
+# Create user 'music' with password PVGMP4F and add it to appropriate groups
 echo "Creating 'music' user and adding it to 'audio' and 'floppy' groups..."
 sudo useradd -m music
 echo "music:PVGMP4F" | sudo chpasswd
 sudo usermod -a -G audio,floppy music
 
-# Download and install VGMPlay
+# Download and install VGMPlay if not already installed
 echo "Downloading and installing VGMPlay..."
 cd ~
-wget 'https://github.com/vgmrips/vgmplay/archive/refs/heads/master.zip'
-unzip master.zip
-cd vgmplay-master/VGMPlay
-make
-sudo make install
-sudo make play_install
-cd ~
+if [ ! -d "vgmplay-master" ]; then
+    wget 'https://github.com/vgmrips/vgmplay/archive/refs/heads/master.zip'
+    unzip master.zip
+    cd vgmplay-master/VGMPlay
+    make
+    sudo make install
+    sudo make play_install
+    cd ~
+fi
 sleep 5
 
-# Add the Python script
-echo "Adding Python script..."
-cat << EOF | sudo tee /usr/local/bin/PVGMP4F.py
+# Add the Python scripts
+echo "Creating player scripts..."
+cat << 'EOF' | sudo tee /usr/local/bin/vgmplay-pvgmp4f.py
 #!/usr/bin/env python3
 
 import argparse
@@ -44,162 +46,132 @@ import logging
 import os
 import subprocess
 import time
-import logging.handlers
 
-FLOPPY_DEV = '/dev/sda'
-FLOPPY_MOUNT = '/mnt/floppy'
-VGZ_PLAYER = '/usr/local/bin/vgmplay'
-SPC_PLAYER = '/usr/bin/mocp'
 SCREEN_SESSION_NAME = 'vgmplay_session'
-
-player_process = None
 
 def get_folder_content(folder_path):
     return [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-def kill_process(process_name):
-    command = ['pgrep', '-f', process_name]
-    try:
-        process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if process.returncode == 0:
-            pids = process.stdout.strip().split('\n')
-            for pid in pids:
-                subprocess.run(['kill', pid])
-            logging.debug(f'{process_name}: {", ".join(pids)} found and terminated.')
-    except Exception as e:
-        logging.exception(f'Error occurred when trying to kill process {process_name}: {e}')
-
-def kill_running_players():
-    global player_process
-    player_process = None
-    processes = ['vgmplay', 'screen', 'mocp']
-    for process in processes:
-        kill_process(process)
-
-def mount_floppy():
-    try:
-        subprocess.run(["sudo", "mount", "-t", "vfat", "-o", "ro", FLOPPY_DEV, FLOPPY_MOUNT])
-        time.sleep(5)  # give the system time to mount
-    except Exception as e:
-        logging.exception(f'Error occurred when trying to mount floppy: {e}')
-
-def unmount_floppy():
-    try:
-        subprocess.run(["sudo", "umount", FLOPPY_MOUNT])
-        time.sleep(5)  # give the system time to unmount
-    except Exception as e:
-        logging.exception(f'Error occurred when trying to unmount floppy: {e}')
-
-def is_process_running(process):
-    if process is None:
-        return False
-    # Check the status of the process; continue if running, else return False
-    process.poll()
-    return process.returncode is None
-
-def is_floppy_mounted():
-    # os.path.ismount will return True if the path is a mount point
-    return os.path.ismount(FLOPPY_MOUNT)
-
-def is_floppy_present():
-    # os.path.exists will return True if the path exists
-    return os.path.exists(FLOPPY_DEV)
-
 def handle_vgz(folder_path):
-    global player_process
     content = get_folder_content(folder_path)
     vgz_files = [f for f in content if f.lower().endswith('.vgz')]
-    logging.debug('Checking for .vgz files.')
     if vgz_files:
-        m3u_files = [f for f in content if f.lower().endswith('.m3u')]
-        if m3u_files:
-            try:
-                logging.debug(f'Starting vgmplay with m3u files: {m3u_files}')
-                command = ['screen', '-dmS', SCREEN_SESSION_NAME, VGZ_PLAYER] + m3u_files
-                player_process = subprocess.Popen(command)
-            except Exception as e:
-                logging.exception(f'Failed to start vgmplay: {e}')
-        else:
-            logging.debug('No .m3u files found.')
+        try:
+            command = ['screen', '-dmS', SCREEN_SESSION_NAME, '/usr/local/bin/vgmplay'] + vgz_files
+            subprocess.run(command, check=True)
+        except Exception as e:
+            logging.exception(f'Failed to start VGMPlay: {e}')
 
-def handle_spc(folder_path):
-    global player_process
-    content = get_folder_content(folder_path)
-    spc_files = [f for f in content if f.lower().endswith('.spc')]
-    logging.debug('Checking for .spc files.')
-    if spc_files:
-        m3u_files = [f for f in content if f.lower().endswith('.m3u')]
-        if m3u_files:
-            try:
-                logging.debug(f'Starting mocp with m3u files: {m3u_files}')
-                command = ['mocp', '-S', '-O', 'AutoNext=yes', '-O', 'Precache=no']  # Start the MOC server
-                subprocess.Popen(command)
-                time.sleep(1)  # Give MOC server time to start
-                command = ['mocp', '-a'] + m3u_files  # Add the music files to the playlist
-                subprocess.Popen(command)
-                time.sleep(1)  # Give MOC some time to add files to the playlist
-                command = ['mocp', '-p']  # Start playing
-                player_process = subprocess.Popen(command)
-            except Exception as e:
-                logging.exception(f'Failed to start mocp: {e}')
-        else:
-            logging.debug('No .m3u files found.')
+def main(folder_path):
+    logging.basicConfig(level=logging.DEBUG)
+    handle_vgz(folder_path)
 
-def main(debug=False):
-    global player_process
-    while True:
-        if is_floppy_present() and not is_floppy_mounted():
-            mount_floppy()
-
-        if is_floppy_mounted():
-            if not is_process_running(player_process):
-                kill_running_players()  # Kill running players before starting
-                handle_vgz(FLOPPY_MOUNT)
-                handle_spc(FLOPPY_MOUNT)
-                # Check if a player process is running after attempting to handle the files
-                if is_process_running(player_process):
-                    while True:
-                        if not is_floppy_present() or not is_floppy_mounted():
-                            kill_running_players()
-                            if is_floppy_mounted():
-                                unmount_floppy()
-                            break
-                        if not is_process_running(player_process):
-                            break
-                        time.sleep(5)
-
-        time.sleep(5)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Music player for floppy disks.')
-    parser.add_argument('-d', '--debug', help='Enable debug logging.', action='store_true')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='VGMPlay player for floppy disks.')
+    parser.add_argument('folder_path', type=str, help='Path to the folder containing the music files.')
     args = parser.parse_args()
-
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.WARNING,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        handlers=[logging.StreamHandler(), logging.handlers.RotatingFileHandler('/var/log/pvgmp4f/PVGMP4F.log', maxBytes=10000, backupCount=3)])
-
-    main(debug=args.debug)
+    main(args.folder_path)
 EOF
 
-# Make the script executable and change the ownership
-echo "Making script executable and changing ownership..."
-sudo chmod +x /usr/local/bin/PVGMP4F.py
-sudo chown music:music /usr/local/bin/PVGMP4F.py
+cat << 'EOF' | sudo tee /usr/local/bin/mocp-pvgmp4f.py
+#!/usr/bin/env python3
+
+import time
+import logging
+import subprocess
+
+SCREEN_SESSION_NAME = 'mocp_session'
+
+def handle_m3u():
+    command = "mocp -S -O AutoNext=yes -O Precache=no && mocp -o r,s,n && mocp -l /mnt/floppy/*"
+    try:
+        subprocess.call(command, shell=True)
+    except Exception as e:
+        logging.exception(f'Failed to start MOC player: {e}')
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    handle_m3u()
+
+if __name__ == '__main__':
+    main()
+EOF
+
+# Make the player scripts executable and change the ownership
+echo "Making player scripts executable and changing ownership..."
+sudo chmod +x /usr/local/bin/vgmplay-pvgmp4f.py
+sudo chmod +x /usr/local/bin/mocp-pvgmp4f.py
+sudo chown music:music /usr/local/bin/vgmplay-pvgmp4f.py
+sudo chown music:music /usr/local/bin/mocp-pvgmp4f.py
+
+# Add the main script
+echo "Creating the main service script..."
+cat << 'EOF' | sudo tee /usr/local/bin/pvgmp4f.sh
+#!/bin/bash
+
+LOGFILE="/var/log/pvgmp4f/pvgmp4f.log"
+FLOPPY_PATH="/mnt/floppy"
+
+echo "$(date) - Script started" | tee -a $LOGFILE
+
+while true; do
+    if [ -b /dev/sda ]; then
+        if ! grep -qs "$FLOPPY_PATH" /proc/mounts; then
+            echo "$(date) - Mounting floppy" | tee -a $LOGFILE
+            sudo mount -t vfat -o ro /dev/sda $FLOPPY_PATH
+            sleep 5
+        fi
+        echo "$(date) - Searching for music files" | tee -a $LOGFILE
+        if ls $FLOPPY_PATH/*.vgz &> /dev/null; then
+            echo "$(date) - VGZ files found" | tee -a $LOGFILE
+            if ! screen -list | grep -q "vgmplay"; then
+                echo "$(date) - Starting vgmplay" | tee -a $LOGFILE
+                screen -dmS vgmplay /usr/local/bin/vgmplay-pvgmp4f.py
+            fi
+        elif ls $FLOPPY_PATH/*.spc &> /dev/null || ls $FLOPPY_PATH/*.m3u &> /dev/null; then
+            echo "$(date) - SPC files or M3U playlist found" | tee -a $LOGFILE
+            if ! ps aux | grep -q "[m]ocp"; then
+                echo "$(date) - Starting moc server" | tee -a $LOGFILE
+                /usr/local/bin/mocp-pvgmp4f.py &>>$LOGFILE
+                sleep 5
+                if ! ps aux | grep -q "[m]ocp"; then
+                    echo "$(date) - Failed to start moc" | tee -a $LOGFILE
+                else
+                    echo "$(date) - MOC state: $(/usr/bin/mocp -i | tee -a $LOGFILE)"
+                fi
+            else
+                echo "$(date) - MOC is already running" | tee -a $LOGFILE
+            fi
+        else
+            echo "$(date) - Music files not found" | tee -a $LOGFILE
+        fi
+    else
+        echo "$(date) - Floppy disk not detected, stopping music" | tee -a $LOGFILE
+        screen -S vgmplay -X quit
+        /usr/bin/mocp -x &>>$LOGFILE
+        sudo umount /mnt/floppy
+    fi
+    echo "$(date) - Sleeping for 5 seconds" | tee -a $LOGFILE
+    sleep 5
+done
+EOF
+
+# Make the main script executable and change the ownership
+echo "Making the main script executable and changing ownership..."
+sudo chmod +x /usr/local/bin/pvgmp4f.sh
+sudo chown music:music /usr/local/bin/pvgmp4f.sh
 
 # Create the service
 echo "Creating the service..."
-cat << EOF | sudo tee /etc/systemd/system/pvgmp4f.service
+cat << EOF | sudo tee /etc/systemd/system/music_player.service
 [Unit]
-Description=Pi Video Game Music Player 4 Floppies
+Description=Music Player for Floppy Disks
 After=multi-user.target
 
 [Service]
 User=music
 Group=music
-ExecStart=/usr/local/bin/PVGMP4F.py
+ExecStart=/usr/local/bin/pvgmp4f.sh
 Restart=on-failure
 
 [Install]
@@ -208,8 +180,8 @@ EOF
 
 # Create the udev rule to ensure the floppy drive is mounted
 echo "Creating udev rule..."
-cat << EOF | sudo tee /etc/udev/rules.d/99-pvgmp4f.rules
-KERNEL=="sda", ACTION=="change", RUN+="/bin/systemctl start pvgmp4f.service"
+cat << EOF | sudo tee /etc/udev/rules.d/99-music-player.rules
+KERNEL=="sda", ACTION=="change", RUN+="/bin/systemctl start music_player.service"
 EOF
 
 # Add 'music' user to sudoers file
@@ -224,17 +196,9 @@ sudo chown music:music /mnt/floppy
 
 # Create log file
 echo "Creating log file and directory..."
-sudo mkdir /var/log/pvgmp4f
-sudo chown music:music /var/log/pvgmp4f
-sudo touch /var/log/pvgmp4f/PVGMP4F.log && sudo chown music:music /var/log/pvgmp4f/PVGMP4F.log
-
-#create MOC config
-sudo mkdir ~/home/.moc
-cat << EOF | sudo tee /home/music/.moc/config
-Precache = no
-AutoNext = yes
-EOF
-chown music:music /home/music/.moc/config
+sudo mkdir /var/log/music_player
+sudo chown music:music /var/log/music_player
+sudo touch /var/log/music_player/music_player.log && sudo chown music:music /var/log/music_player/music_player.log
 
 # Reload the udev rules
 echo "Reloading udev rules..."
@@ -244,8 +208,8 @@ sleep 5
 # Enable and start the service
 echo "Enabling and starting the service..."
 sudo systemctl daemon-reload
-sudo systemctl enable pvgmp4f.service
-sudo systemctl start pvgmp4f.service
+sudo systemctl enable music_player.service
+sudo systemctl start music_player.service
 
 echo "Installation completed successfully!"
 exit 0
--- uninstaller.sh
+++ uninstaller.sh
@@ -1,44 +1,61 @@
 #!/bin/bash
 
-# Check if the script is running with sudo
+# Check if the script is running as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run with sudo. Please try again with 'sudo'."
+   echo "This script must be run as root. Please try again with 'sudo'."
    exit 1
 fi
 
-# Stop and disable the music player service
-echo "Stopping and disabling the music player service..."
+# Stop the service
+echo "Stopping the service..."
 sudo systemctl stop music_player.service
+
+# Disable the service
+echo "Disabling the service..."
 sudo systemctl disable music_player.service
 
-# Remove the music player service file
-echo "Removing the music player service file..."
+# Remove the service file
+echo "Removing the service file..."
 sudo rm /etc/systemd/system/music_player.service
 
 # Remove the udev rule
 echo "Removing the udev rule..."
 sudo rm /etc/udev/rules.d/99-music-player.rules
 
-# Remove the music player script
-echo "Removing the music player script..."
-sudo rm /usr/local/bin/music_player.py
+# Remove the user from sudoers file
+echo "Removing 'music' user from sudoers file..."
+sudo sed -i '/music ALL=NOPASSWD: \/usr\/bin\/mount/d' /etc/sudoers
+sudo sed -i '/music ALL=NOPASSWD: \/usr\/bin\/umount/d' /etc/sudoers
 
-# Remove the player scripts
-echo "Removing the player scripts..."
+# Remove the scripts
+echo "Removing the scripts..."
 sudo rm /usr/local/bin/vgmplay-pvgmp4f.py
 sudo rm /usr/local/bin/mocp-pvgmp4f.py
+sudo rm /usr/local/bin/pvgmp4f.sh
 
-# Remove the log directory and log file
-echo "Removing the log directory and log file..."
-sudo rm -rf /var/log/music_player
+# Remove the log file and directory
+echo "Removing log file and directory..."
+sudo rm -r /var/log/music_player
 
-# Remove the floppy directory
-echo "Removing the floppy directory..."
-sudo rm -rf /mnt/floppy
+# Remove the directory for floppy
+echo "Removing directory for floppy..."
+sudo rm -r /mnt/floppy
+
+# Uninstall VGMPlay
+echo "Uninstalling VGMPlay..."
+cd ~/vgmplay-master/VGMPlay
+sudo make uninstall
+cd ~
+sudo rm -rf vgmplay-master
+
+# Remove 'music' user
+echo "Removing 'music' user..."
+sudo userdel -r music
 
 # Reload the udev rules
-echo "Reloading the udev rules..."
+echo "Reloading udev rules..."
 sudo udevadm control --reload-rules
+sleep 5
 
 echo "Uninstallation completed successfully!"
 exit 0
